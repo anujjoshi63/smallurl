90d8013f722ac2dc1c520d46a2aa371b76524e48
Anuj <anujjoshi63@gmail.com>
2022-07-24 17:17:42 +0530

    add middleware to redirect if request isnt from / or /api

--- src/pages/api/url/[slug].ts
+++ src/pages/api/url/[slug].ts
@@ -1,18 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
 import { prisma } from '../../../db/client';
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const slug = req.query['slug'];
 
 	if (!slug || typeof slug !== 'string') {
 		res.statusCode = 404;
+
 		res.send(JSON.stringify({ message: 'pls use with a slug' }));
+
 		return;
 	}
+
 	const data = await prisma.shortUrl.findFirst({
 		where: {
-			slug: {
-				equals: slug
-			}
+			slug
 		}
 	});
 
@@ -23,5 +26,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 		return;
 	}
-	return res.redirect(data.url);
+
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Cache-Control',
+		's-maxage=1000000000, stale-while-revalidate'
+	);
+
+	return res.json(data);
 };
