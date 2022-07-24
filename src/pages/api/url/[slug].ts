707ce6543033bc911aa51fc546191ff1546703c5
Anuj <anujjoshi63@gmail.com>
2022-07-24 15:53:08 +0530

    basic working structure

---
+++ src/pages/api/url/[slug].ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../db/client';
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const slug = req.query['slug'];
+
+	if (!slug || typeof slug !== 'string') {
+		res.statusCode = 404;
+		res.send(JSON.stringify({ message: 'pls use with a slug' }));
+		return;
+	}
+	const data = await prisma.shortUrl.findFirst({
+		where: {
+			slug: {
+				equals: slug
+			}
+		}
+	});
+
+	if (!data) {
+		res.statusCode = 404;
+
+		res.send(JSON.stringify({ message: 'slug not found' }));
+
+		return;
+	}
+	return res.redirect(data.url);
+};
