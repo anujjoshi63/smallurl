b0cc6b8aa6cbbc69b6ff23d249a585cf8c1d1a18
Anuj <anujjoshi63@gmail.com>
2022-07-26 00:40:34 +0530

    update middleware

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,17 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
-	const nextPath = req.nextUrl.pathname;
-	if (!nextPath.startsWith('/api/') && nextPath !== '/') {
-		const slug = nextPath.split('/').pop();
-		const slugFetch = await fetch(`${req.nextUrl.origin}/api/url/${slug}`);
-		if (slugFetch.status === 404) {
-			return NextResponse.redirect(req.nextUrl.origin);
-		}
-		const data = await slugFetch.json();
-		if (data?.url) {
-			return NextResponse.redirect(data.url);
-		}
-		console.log('in middleware');
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+	if (
+		req.nextUrl.pathname.startsWith('/api/') ||
+		req.nextUrl.pathname === '/'
+	) {
+		return;
+	}
+	const slug = req.nextUrl.pathname.split('/').pop();
+	const slugFetch = await fetch(`${req.nextUrl.origin}/api/url/${slug}`);
+	if (slugFetch.status === 404) {
+		return NextResponse.redirect(req.nextUrl.origin);
+	}
+	const data = await slugFetch.json();
+
+	if (data?.url) {
+		return NextResponse.redirect(data.url);
 	}
 }
